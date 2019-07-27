8d8ef172484e76a3feeb3478d188e691cb30ef2d
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-27 19:08:16 -0300

    Finalized project

---
+++ src/components/layout/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+        </header>
+    )
+}
+
+//Header style
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
+
+//Link style
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
+}
+
+export default Header;
